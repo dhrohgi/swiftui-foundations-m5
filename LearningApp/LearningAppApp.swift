bd7c45dd709386546a96ffc3cd2e761d82c797fb
Daehan Roh <dhrohgo@gmail.com>
2021-10-10 15:32:43 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by dennis on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
