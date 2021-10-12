e086286d633ad49bc77665ae8bfd07b0fe666b5d
Daehan Roh <dhrohgo@gmail.com>
2021-10-11 20:42:28 -0500

    - Rename content view - Organize files into folders - Add content model - Set content model as environment object

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,16 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by dennis on 10/10/21.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+    
+    
+}
